fa394c8c3a79be2417062a976f99362a029280c9
Olivia Wu <qinyuan.wupsn@gmail.com>
2020-11-16 14:55:08 +0800

    CRUD deployed with async feature

--- model/CourseDao.js
+++ model/CourseDao.js
@@ -1,27 +1,44 @@
 const Course = require("./Course.js");
 
+//deploy CRUD operations
+//use built-in functions of Course model to access data
 class CourseDao {
 	constructor() {
 	}
 
-	async create() {
-
+	//create a course document
+	async create(title, number, term, status) {
+		const course = await Course.create({ title, number, term, status });
+		return course;
 	}
 
-	async readAll() {
-
+	//return all courses matching the specified status
+	//if status not specified, return all courses
+	async readAll(status = "") {
+		const filter = status ? { status } : {};
+		const courses = await Course.find(filter);
+		return courses;
 	}
 
-	async read() {
-
+	//return the course with the matching id
+	async read(id) {
+		const course = await Course.findById(id);
+		return course;
 	}
 
-	async update() {
-
+	//validate and update the given status of a course with the matching id
+	async update(id, status) {
+		const course = await Course.findByIdAndUpdate(
+			id,
+			{ status },
+			{ new: true, runValidators: true}
+		);
+		return course;
 	}
 
-	async delete() {
-
+	async delete(id) {
+		const course = await Course.findByIdAndDelete(id);
+		return course;
 	}
 }
 
