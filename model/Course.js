f27f0a260fce20a146e20b0157d34127eded4072
Olivia Wu <qinyuan.wupsn@gmail.com>
2020-11-16 11:16:12 +0800

    mongoose course attributes validation added, Dao skeleton code created

--- model/Course.js
+++ model/Course.js
@@ -2,10 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema({
-  title: { type: String, required: true },
-  number: { type: String, required: true },
-  term: { type: String, required: true },
-  status: { type: String, required: true }, //"taken", "enrolled", or "interested"
+  title: { 
+    type: String, 
+    required: [true, "Path `title` is required."] 
+  },
+  number: { 
+    type: String, 
+    required: [true, "Path `number` is required."] 
+  },
+  term: { 
+    type: String, 
+    required: [true, "Path `term` is required."] 
+  },
+  status: { 
+    type: String, 
+    required: [true, "Path `status` is required."], 
+    enum: ["taken", "enrolled", "interested"]}, //only accepts these status
 });
 
 const Course = mongoose.model("Course", CourseSchema);
---
+++ model/CourseDao.js
@@ -0,0 +1,28 @@
+const Course = require("./Course.js");
+
+class CourseDao {
+	constructor() {
+	}
+
+	async create() {
+
+	}
+
+	async readAll() {
+
+	}
+
+	async read() {
+
+	}
+
+	async update() {
+
+	}
+
+	async delete() {
+
+	}
+}
+
+module.exports = CourseDao;
